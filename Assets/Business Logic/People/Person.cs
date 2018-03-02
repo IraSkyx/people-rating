46050a6604e23faaaafb6efdf645db67448f11cc
Lenoir Adrien <planete.skyx@gmail.com>
2018-03-02 15:03:20 +0100

    Initial commit

---
+++ Assets/Business Logic/People/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Business_Logic.People
+{
+    [Serializable]
+    public class Person
+    {
+        public string completeName;
+
+        public string age;
+
+        public string occupation;
+
+        public string comment;
+
+        public Person(string completeName, string age, string occupation, string comment)
+        {
+            this.completeName = completeName;
+            this.age = age;
+            this.occupation = occupation;
+            this.comment = comment;
+        }
+
+        public override string ToString()
+        {
+            return $"{completeName}\n{age}\n{occupation}\n{comment}\n";
+        }
+    }
+}
---
+++ Assets/Loop.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Business_Logic.People;
+
+public class Loop : MonoBehaviour
+{
+    public Text completeName;
+    public Text age;
+    public Text occupation;
+    public Text comment;
+    public Text ageLabel;
+    public Text occupationLabel;
+
+    public Person currentPerson;
+
+    // Use this for initialization
+    void Start()
+    {
+        UnassignValues();
+        InvokeRepeating("ReadQR", 1f, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void ReadQR()
+    {
+#if !UNITY_EDITOR
+        try 
+        {
+            MediaFrameQrProcessing.Wrappers.ZXingQrCodeScanner.ScanFirstCameraForQrCode(
+                       result =>
+                       {
+                           UnityEngine.WSA.Application.InvokeOnAppThread(() =>
+                           {                               
+                               if (result != null)
+                               {
+                                    currentPerson = JsonUtility.FromJson<Person>(result.Text);
+                                    AssignValues();
+                               }
+                               else
+                               {
+                                    UnassignValues();
+                               }
+                           },
+                           false);
+                       },
+                       TimeSpan.FromSeconds(1.4));
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+#endif
+    }
+
+    public void AssignValues()
+    {
+        if (currentPerson != null)
+        {
+            completeName.text = currentPerson.completeName;
+            age.text = currentPerson.age;
+            occupation.text = currentPerson.occupation;
+            comment.text = currentPerson.comment;
+            ageLabel.text = "Age:";
+            occupationLabel.text = "Occupation:";
+        }
+    }
+
+    private void UnassignValues()
+    {
+        completeName.text = String.Empty;
+        age.text = String.Empty;
+        occupation.text = String.Empty;
+        comment.text = String.Empty;
+        ageLabel.text = String.Empty;
+        occupationLabel.text = String.Empty;
+    }
+}
